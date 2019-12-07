a29c4c4145b0702da7b00c4c55ad3330622a854f
DjCooGie <evansonchagwa01@gmail.com>
2019-12-07 23:27:58 +0300

    imported profile-service folder, FormsModule and HttpClientModule. Added HttpClientModule and FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileService } from './profile.service'
+import { FormsModule }from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { ProfileService } from './profile.service'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
