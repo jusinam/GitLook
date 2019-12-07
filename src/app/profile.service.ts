efe699b2665e4852295afde9787b6dd87f7e6e66
DjCooGie <evansonchagwa01@gmail.com>
2019-12-07 18:55:00 +0300

    added three private properties in the class ProfileService object

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient }from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+
+  private username = "string";
+  private client_id = "6b94f580e9f01fb4dd66";
+  private client_secret = "efcde45be31e4fe94664006f38d36d3a5e26f06a";
 
   constructor() { }
 }
