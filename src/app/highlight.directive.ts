c6f41e91da59c1db45fa75eb459e8e6e681ef545
DjCooGie <evansonchagwa01@gmail.com>
2019-12-09 10:04:32 +0300

    ipmorted hostlistener and elementref and two hostlistener handlers adorned by a decorator in the directive ts file, applied the directive attribute in the profile index file, modified some styling

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
+
+  @HostListener ('mouseenter') onMouseEnter() {
+    this.highlight('skyblue');
+  }
+
+  @HostListener ('mouseleave') onMouseLeave() {
+    this.highlight('null');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+
+  }
+
+   
 
 }
